71a87b8371d5d9539bdcb66dded390138b2bcc85
Nitin-Salunke <nitin@39shops.com>
2016-05-20 02:30:03 +0530

    Setup API methods.

--- lib/postmen/carrier.rb
+++ lib/postmen/carrier.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'net/http'
+
 module Postmen
   class Carrier
 
@@ -8,6 +11,31 @@ module Postmen
       @api_key = options[:api_key]
       @mode = options[:mode]
       @api_url = get_api_url(options[:mode])
+      @async = options[:async]
+    end
+
+    def calculate_rates
+    end
+
+    def retrieve_rates_by_id
+    end
+
+    def create_label
+    end
+
+    def retrieve_label_by_id
+    end
+
+    def list_all_label
+    end
+
+    def cancel_label
+    end
+
+    def list_all_cancel_labels
+    end
+
+    def retrieve_cancel_label_by_id
     end
 
     private
@@ -20,6 +48,20 @@ module Postmen
         else
           ''
       end
+    end
+
+
+    def process_request(url, body = nil)
+      url = URI(url)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(url)
+      request['postmen-api-key'] = '8fc7966b-679b-4a57-911d-c5a663229c9e'
+      request['content-type'] = 'application/json'
+      request.body = body if body.present?
+
+      http.request(request)
     end
   end
 end
