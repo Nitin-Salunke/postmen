bad52774ffb776febeeff770051a74d7a4205e2e
Nitin-Salunke <nitin@39shops.com>
2016-05-30 14:18:24 +0530

    Add fedex service type.

--- lib/postmen/carriers/fedex.rb
+++ lib/postmen/carriers/fedex.rb
@@ -10,6 +10,8 @@ module Postmen
     PARCEL ={
         :box_type => %w(custom fedex_10kg_box fedex_25kg_box fedex_box fedex_envelope fedex_extra_large_box fedex_large_box fedex_medium_box fedex_pak fedex_small_box fedex_tube)
     }
+    
+    SERVICE_TYPE = %w(fedex_2_day fedex_2_day_am fedex_2_day_am_one_rate fedex_2_day_one_rate fedex_distance_deferred fedex_europe_first_international_priority fedex_express_saver fedex_express_saver_one_rate fedex_first_overnight fedex_first_overnight_one_rate fedex_ground fedex_ground_home_delivery fedex_international_economy fedex_international_first fedex_international_priority fedex_next_day_afternoon fedex_next_day_early_morning fedex_next_day_end_of_day fedex_next_day_mid_morning fedex_priority_overnight fedex_priority_overnight_one_rate fedex_same_day fedex_same_day_city fedex_standard_overnight fedex_standard_overnight_one_rate)
 
     def initialize(options)
       super
