ba66f10f218252f686eae34f8877388fd6c7e840
Nitin-Salunke <nitin@39shops.com>
2016-08-02 01:48:38 +0530

    Removed fedex initialize method.

--- lib/postmen/carriers/fedex.rb
+++ lib/postmen/carriers/fedex.rb
@@ -1,8 +1,6 @@
 module Postmen
   class Fedex < Carrier
 
-    attr_reader :billing_account_number
-
     BILLING = {
         :paid_by => %W(shipper recipient third_party)
     }
@@ -12,11 +10,6 @@ module Postmen
     }
     
     SERVICE_TYPE = %w(fedex_2_day fedex_2_day_am fedex_2_day_am_one_rate fedex_2_day_one_rate fedex_distance_deferred fedex_europe_first_international_priority fedex_express_saver fedex_express_saver_one_rate fedex_first_overnight fedex_first_overnight_one_rate fedex_ground fedex_ground_home_delivery fedex_international_economy fedex_international_first fedex_international_priority fedex_next_day_afternoon fedex_next_day_early_morning fedex_next_day_end_of_day fedex_next_day_mid_morning fedex_priority_overnight fedex_priority_overnight_one_rate fedex_same_day fedex_same_day_city fedex_standard_overnight fedex_standard_overnight_one_rate)
-
-    def initialize(options)
-      super
-      @billing_account_number = options[:billing_account_number]
-    end
 
     def retrieve_rates_by_id
       raise NotImplementedError  "Method: retrieve_rates_by_id is not supported by #{self.class.name}."
